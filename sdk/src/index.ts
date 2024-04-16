f30225eabbad5f6f371d48fa7608528ea378d5d4
Wayne Cheng <677680+welps@users.noreply.github.com>
2024-04-16 00:15:37 -0400

    feat: add slippage endpoint

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,6 +1,7 @@
 export * from './wrappedAsset';
 export * from './types';
 export * from './quotes';
+export * from './slippage';
 export * from './utils/constants';
 export * from './utils/permit';
 export * from './utils/sources';
