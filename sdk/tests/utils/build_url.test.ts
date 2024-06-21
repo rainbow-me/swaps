6a44fd6d01dc22c09780416e96c10b81cb29f53a
Benjamín Cárcamo <fringlesinthestreet@users.noreply.github.com>
2024-06-21 11:42:56 -0400

    feat: exposing relay source on crosschain swaps (#76)
    * feat: exposing relay source on crosschain swaps
    
    * fix: lint

--- sdk/tests/utils/build_url.test.ts
+++ sdk/tests/utils/build_url.test.ts
@@ -1,4 +1,7 @@
-import { buildRainbowCrosschainQuoteUrl } from '../../src';
+import {
+  buildRainbowClaimBridgeQuoteUrl,
+  buildRainbowCrosschainQuoteUrl,
+} from '../../src';
 
 describe('when creating crosschain swap URL', () => {
   it('should consider feePercentageBasisPoints if passed', () => {
@@ -33,6 +36,25 @@ describe('when creating crosschain swap URL', () => {
       })
     ).toEqual(
       'https://swap.p.rainbow.me/v1/quote?bridgeVersion=3&buyToken=0x456&chainId=1&fromAddress=0x789&refuel=false&sellAmount=100&sellToken=0x123&slippage=2&swapType=cross-chain&toChainId=137'
+    );
+  });
+});
+
+describe('when creating claim bridge swap URL', () => {
+  it('should set feePercentageBasisPoints to 0 and claim as true if passed', () => {
+    expect(
+      buildRainbowClaimBridgeQuoteUrl({
+        buyTokenAddress: '0x456',
+        chainId: 1,
+        fromAddress: '0x789',
+        refuel: false,
+        sellAmount: '100',
+        sellTokenAddress: '0x123',
+        slippage: 2,
+        toChainId: 137,
+      })
+    ).toEqual(
+      'https://swap.p.rainbow.me/v1/quote?bridgeVersion=3&buyToken=0x456&chainId=1&claim=true&feePercentageBasisPoints=0&fromAddress=0x789&refuel=false&sellAmount=100&sellToken=0x123&slippage=2&source=relay&swapType=cross-chain&toChainId=137'
     );
   });
 });
